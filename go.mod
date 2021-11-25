f664cbb747cb75c51c00d355eca73f462f968452
novaguinea <novaguinea.a@gmail.com>
2021-11-26 01:18:22 +0700

    --

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.17
 
 require (
 	github.com/gin-gonic/gin v1.7.7
-	github.com/joho/godotenv v1.4.0
 	gorm.io/driver/mysql v1.2.0
 	gorm.io/gorm v1.22.3
 )
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import(
 	"net/http"
 	"github.com/gin-gonic/gin"
 	// "gorm.io/gorm"
-	"github.com/novaguinea/findor/models"
+	"github.com/novaguinea/findor/database"
 )
 
 func main()  {
@@ -12,6 +12,10 @@ func main()  {
 	r := gin.Default()
 
 	db := database.SetupModels()
+
+	r.Use(func(c *gin.Context)  {
+		c.Set("db", db)
+	})
 
 	r.GET("/", func(c *gin.Context)  {
 		c.JSON(http.StatusOK, gin.H{"status":"hehe hello"})
