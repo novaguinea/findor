6d49a75aaa4f4e60855dd344072e00df3e2d3efd
novaguinea <novaguinea.a@gmail.com>
2021-11-29 22:58:55 +0700

    aaaaaaaaaaaaaaaaaaaaa

--- models/users.go
+++ models/users.go
@@ -1,11 +1,5 @@
 package models
 
-import(
-	// "gorm.io/gorm"
-	"time"
-
-)
-
 type Users struct {
 	ID			uint32		`json:"id" gorm:"primary_key:auto_increment"`
 	Name		string		`json:"name" gorm:"size:255;not null"`
@@ -17,6 +11,4 @@ type Users struct {
 	Age			int			`json:"age" gorm:"size:255"`
 	IsAvailable	bool		`json:"isAvailable" gorm:"size:255;default:true"`
 	AvatarURL	string		`json:"avatarUrl" gorm:"size:255;default:https://avatars.dicebear.com/api/personas/default.svg"`
-	CreateAt	time.Time	`json:"create_at" gorm:"default:CURRENT_TIMESTAMP"`
-	UpdateAt	time.Time	`json:"update_at" gorm:"default:CURRENT_TIMESTAMP"`	
 }
